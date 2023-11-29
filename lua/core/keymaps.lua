848de550f8ba8d99115e7f6effe5b1dd330cf2ec
clement32112 <totoobieke@gmail.com>
2023-11-29 08:05:14 +0100

    Completed basic keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -15,7 +15,7 @@ function test()
 end
 
 kmap("n","<Leader>`", "<cmd>lua test()<CR>",no_opts )
-kmap("n", "<Leader><Enter>", "<cmd> exe 'normal a'.nr2char(getchar())<CR><CR>",opts) -- remmaping enter to neline in normalmode
+kmap("n", "<Leader><Enter>", "<cmd> exe 'normal a'.nr2char(getchar())<CR><CR>",opts) -- remmaping enter to newline in normalmode
 
 kmap("n", "<Up>","g<Up>",no_opts) -- move up even if line wrapped
 kmap("n", "<Down>","g<Down>",no_opts) -- move down even if line wrapped
@@ -23,15 +23,16 @@ kmap("n", "<Down>","g<Down>",no_opts) -- move down even if line wrapped
 kmap("n", "<M-Up>","<cmd> move -2 <CR>",opts)-- Move line up
 kmap("n","<M-Down>","<cmd> move +1<CR>",opts)-- Move line down
 
+--kmap ("v", "<M-Up>", "<cmd> '<,'> move -2 <CR>",opts) -- move selecion up
+--kmap ("v", "<M-Down>", "<cmd> '<,'> move +1 <CR>",opts) -- move selecion up
 
--- kmap ("v", "<M-Up>", "<cmd> '<,'> move -2 <CR>",opts) -- move selecion up
--- kmap ("v", "<M-Down>", "<cmd> '<,'> move +1 <CR>",opts) -- move selecion up
-
-kmap("n", "<Leader>t", "<cmd>tabn<CR>", opts)
-kmap("n", "<Leader>T", "<cmd>tabp<CR>",opts)
+kmap("n", "<Leader>t", "<cmd>tabn<CR>", opts) -- Go to the next tab
+kmap("n", "<Leader>T", "<cmd>tabp<CR>",opts) -- Go to the previous tab
 
 kmap("n", "<C-UP>", "3<C-y>",opts) -- scroll down more quickly
 kmap("n", "<C-DOWN>", "3<C-e>",opts) -- scroll up more quickly
 
 kmap("n", "<Leader><tab>", "<cmd> tabnew<CR>",opts) -- create new tab
 kmap("n", "<tab>", "<C-w>", opts)
+
+kmap("t", "<Esc><Esc>", [[<C-\><C-n>]],opts) --exit terminal mode :)
